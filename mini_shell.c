6a6097c9b1282a48a8e5d0632b5be786c0913128
valera arnovich <61805285+valera1w21@users.noreply.github.com>
2026-09-21 00:47:12 +0300

    refactor mini_shell.c: replace execlp with execvp for improved command execution and enhance argument handling

--- mini_shell.c
+++ mini_shell.c
@@ -11,17 +11,27 @@ int main() {
         printf("> ");
         fgets(buffer, 1024, stdin);
         buffer[strlen(buffer) - 1] = '\0';
+
+        char *slova[64];
+        int n = 0;
+
+        char *slovo = strtok(buffer, " ");
+        while (slovo != NULL) {
+            slova[n] = slovo;
+            n++;
+            slovo = strtok(NULL, " ");
+        }
+        slova[n] = NULL;
+
         int rezultat = fork();
-
-    if (rezultat == 0) {
-        execlp(buffer, buffer, NULL);
-        perror("execlp");
-        exit(1);
-    } else {
-        int status;
-        wait(&status); 
-       }
-
+        if (rezultat == 0) {
+            execvp(slova[0], slova);
+            perror("execvp");
+            exit(1);
+        } else {
+            int status;
+            wait(&status);
+        }
     }
 
     return 0;
