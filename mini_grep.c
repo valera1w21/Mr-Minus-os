c1d0399c7e820319899380df3cdb1fe52fa54bb9
valera arnovich <61805285+valera1w21@users.noreply.github.com>
2026-09-20 16:35:12 +0300

    refactor mini_grep.c: update usage message and simplify file reading logic

--- mini_grep.c
+++ mini_grep.c
@@ -1,58 +1,34 @@
 #include <stdio.h>
-#include <ctype.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
 
-    if (argc < 2) {
-          printf("Usage: %s filename\n", argv[0]);
+    if (argc < 3) {
+          printf("Usage: %s pattern filename\n", argv[0]);
     return 1;
     }
 
 int errory = 0;
 
-  for(int i = 1; i < argc; ++i){
 
-    int symbols = 0;
-    int lines = 0;
-    int words = 0;
-    int v_lesu = 0;
-
-  FILE *f = fopen(argv[i], "r");
+  FILE *f = fopen(argv[2], "r"); //I HATE ARGV
 
    if (f == NULL) {
     perror("fopen");
     errory = 1;
-    continue;
-    
+    return errory;
 }
 
+  char buffer[1024];
+  while (fgets(buffer, 1024, f) != NULL){ 
 
-
-  int ch;
-   while ((ch = fgetc(f)) != EOF) {
-   symbols++;
-
-     if (isspace(ch)) {
-    v_lesu = 0;
+  if (strstr(buffer, argv[1]) != NULL){ 
+    printf("%s", buffer);
+    }
 }
 
-  if (!isspace(ch) && v_lesu == 0) {
-    words++;
-    v_lesu = 1;
-}
-
-
-   if (ch == '\n') {
-    lines++;
-}
-
-  }
-
-
-   printf("%d %d %d %s\n", lines, words, symbols, argv[i]);
    fclose(f);
-  }
 
      return errory;
 }
