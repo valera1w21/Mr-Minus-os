59684d408dc12257a47fe11481b4edd2123d54d4
valera arnovich <61805285+valera1w21@users.noreply.github.com>
2026-09-20 18:02:12 +0300

    refactor mini_fork.c: streamline child process handling by removing unnecessary output and adding error handling for execlp

--- mini_fork.c
+++ mini_fork.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int main() {
     printf("do\n");
     int rezultat = fork();
+
     if (rezultat == 0) {
-        printf("child\n");
+        execlp("ls", "ls", NULL);
+        perror("execlp");
+        exit(1);
     } else {
         printf("parent\n");
+
     }
     return 0;
 }
